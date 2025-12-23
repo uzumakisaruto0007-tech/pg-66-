7c0c2bb719e329a5913212e7289b808a0d0b2fdc
uzumakisaruto0007-tech <uzumakisaruto0007@gmail.com>
2025-12-23 12:09:46 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let difference = 0
+let Sum = 0
+function diff (num1: number, num2: number) {
+    difference = num1 - num2
+    return difference
+}
+function SUM (num1: number, num2: number) {
+    Sum = num1 + num2
+    return Sum
+}
